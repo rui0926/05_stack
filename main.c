7557ce43711d05ef3c5d88bd382c42a1bd0b0530
rui0926 <43432504+rui0926@users.noreply.github.com>
2018-10-24 16:02:03 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,11 +1,39 @@
 #include <stdio.h>
 
+#define stack_size 10
+
 int main(int argc, char *argv[]) 
 {
     char str[7];
+    int array[100];
+    int a
     
-    while(fgets(str, sizeof(str), stdin)){
-        printf("%s", str);
+    
+    while(fgets(str, sizeof(str), stdin))
+    {
+        a = 0;
+        int input = atoi(str); 
+        
+        if(input == 0)
+        {
+         for(a = 0; a > stack_size; a++)
+         {
+          printf("%d",stack[a]);
+             for()
+         }
+        }
+        
+        else if(input == -1)
+        {
+            printf("%d\n",stack[a]);
+            a--;
+        }
+        
+        else
+        {
+            
+        }
+    
     }
 
     return 0;
